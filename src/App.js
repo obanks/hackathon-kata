e9eff0144aa6eb4841238630d54539fa097e2a00
Oliver Banks <oliver.banks@verint.com>
2019-11-29 15:11:45 +0000

    create test cases

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
 import React from 'react';
 import './App.css';
+import {TextField} from '@material-ui/core';
+
+class App extends React.Component {
+
+    calculate = (value) => {
+        let output = 0;
+        let scores = value.split('');
+        scores.forEach(function (score) {
+            if (score !== '-') {
+                output += parseInt(score);
+            }
+        });
+        return output;
+    };
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: '',
+        };
+        this.onChange = this.onChange.bind(this);
+    }
 
 function App() {
   return (
